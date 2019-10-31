c3a02d9383c1a4c37a1da5a14e29ebaff9650448
kgalexis <phadomduck@gmail.com>
2019-10-31 15:11:33 +0200

    Add files via upload

--- LGM-Classification/adjacency_features.py
+++ LGM-Classification/adjacency_features.py
@@ -1,10 +1,22 @@
 import numpy as np
+from shapely.geometry import Point
 import pickle
 
 from sklearn.neighbors import KDTree
 
 
 def create_poi_index(poi_gdf, path):
+    """
+    Creates spatial index containing the pois given.
+
+    Args:
+        poi_gdf (geopandas.GeoDataFrame) : Contains pois to be stored in the \
+            index
+        path (str): Path to save the index
+
+    Returns:
+        None
+    """
     poi_coords = poi_gdf[['lon', 'lat']].values
     poi_index = KDTree(poi_coords)
     pickle.dump(poi_index, open(path, 'wb'))
@@ -12,6 +24,23 @@ def create_poi_index(poi_gdf, path):
 
 
 def get_classes_in_radius_bln(poi_gdf, poi_index_path, nlabels, label_map, thr):
+    """
+    Creates a features array. For each poi *p* (each row) the array will
+    contain 1 (True) in column *c*, if there is at least one poi of category *c*
+    inside *p*'s defined radius.
+
+    Args:
+        poi_gdf (geopandas.GeoDataFrame): Contains pois for which the \
+            features will be created
+        poi_index_path (str): Path to the stored index
+        nlabels (int): Number of poi categories
+        label_map (list): A list containing the labels of the train pois
+        thr (float): Radius to be searched (in meters)
+
+    Returns:
+        numpy.ndarray: The features array of shape (n_samples, n_features), \
+            here (len(poi_gdf), nlabels)
+    """
     poi_index = pickle.load(open(poi_index_path, 'rb'))
     X = np.zeros((len(poi_gdf), nlabels))
     for poi in poi_gdf.itertuples():
@@ -24,6 +53,23 @@ def get_classes_in_radius_bln(poi_gdf, poi_index_path, nlabels, label_map, thr):
 
 
 def get_classes_in_radius_cnt(poi_gdf, poi_index_path, nlabels, label_map, thr):
+    """
+    Creates a features array. For each poi *p* (each row) the array will
+    contain an integer in column *c*, representing the number of pois of
+    category *c* inside *p*'s defined radius.
+
+    Args:
+        poi_gdf (geopandas.GeoDataFrame): Contains pois for which the \
+            features will be created
+        poi_index_path (str): Path to the stored index
+        nlabels (int): Number of poi categories
+        label_map (list): A list containing the labels of the train pois
+        thr (float): Radius to be searched (in meters)
+
+    Returns:
+        numpy.ndarray: The features array of shape (n_samples, n_features), \
+            here (len(poi_gdf), nlabels)
+    """
     poi_index = pickle.load(open(poi_index_path, 'rb'))
     X = np.zeros((len(poi_gdf), nlabels))
     for poi in poi_gdf.itertuples():
@@ -39,13 +85,41 @@ def get_classes_in_radius_cnt(poi_gdf, poi_index_path, nlabels, label_map, thr):
 
 
 def get_classes_in_street_and_radius_bln(poi_gdf, street_gdf, pois_by_street, nlabels, label_map, geometry_map, thr):
+    """
+    Creates a features array. For each poi *p*, the nearest street to *p* is \
+    identified and the pois of this street are kept. These pois are then \
+    filtered and only those which are inside *p*'s defined radius are \
+    considered (e.g. a set of pois *P*). Finally, for each poi *p* (each row) \
+    the array will contain 1 (True) in column *c*, if there is at least one \
+    poi of category *c* among pois in *P*.
+
+    Args:
+        poi_gdf (geopandas.GeoDataFrame): Contains pois for which the \
+            features will be created
+        street_gdf (geopandas.GeoDataFrame): Contains all streets extracted \
+            from OSM, along with their geometries
+        pois_by_street (dict): Has streets ids as keys and a list containing \
+            the pois which belong to each street as values
+        nlabels (int): Number of poi categories
+        label_map (list): A list containing the labels of the train pois
+        geometry_map (list): A list containing the geometries of the train pois
+        thr (float): Radius to be searched (in meters)
+
+    Returns:
+        numpy.ndarray: The features array of shape (n_samples, n_features), \
+            here (len(poi_gdf), nlabels)
+    """
     street_index = street_gdf.sindex
     X = np.zeros((len(poi_gdf), nlabels))
     for poi in poi_gdf.itertuples():
         poi_coords = (poi.lon, poi.lat)
-        result_street_idx = list(street_index.nearest(poi_coords))[0]
+        candidates = list(street_index.nearest(poi_coords))
+        nearest = candidates[np.argmin([
+            Point(poi_coords).distance(street_gdf.iloc[c]['geometry'])
+            for c in candidates
+        ])]
         result_pois_idxs = [
-            poi_idx for poi_idx in pois_by_street[result_street_idx]
+            poi_idx for poi_idx in pois_by_street[nearest]
             if poi.geometry.distance(geometry_map[poi_idx]) < thr]
         for rpi in result_pois_idxs:
             rpi_label = label_map[rpi]
@@ -54,14 +128,42 @@ def get_classes_in_street_and_radius_bln(poi_gdf, street_gdf, pois_by_street, nl
 
 
 def get_classes_in_street_and_radius_cnt(poi_gdf, street_gdf, pois_by_street, nlabels, label_map, geometry_map, thr):
+    """
+    Creates a features array. For each poi *p*, the nearest street to *p* is \
+    identified and the pois of this street are kept. These pois are then \
+    filtered and only those which are inside *p*'s defined radius are \
+    considered (e.g. a set of pois *P*). Finally, for each poi *p* (each row) \
+    the array will contain an integer in column *c*, representing the number \
+    of pois of category *c* among pois in *P*.
+
+    Args:
+        poi_gdf (geopandas.GeoDataFrame): Contains pois for which the \
+            features will be created
+        street_gdf (geopandas.GeoDataFrame): Contains all streets extracted \
+            from OSM, along with their geometries
+        pois_by_street (dict): Has streets ids as keys and a list containing \
+            the pois which belong to each street as values
+        nlabels (int): Number of poi categories
+        label_map (list): A list containing the labels of the train pois
+        geometry_map (list): A list containing the geometries of the train pois
+        thr (float): Radius to be searched (in meters)
+
+    Returns:
+        numpy.ndarray: The features array of shape (n_samples, n_features), \
+            here (len(poi_gdf), nlabels)
+    """
     street_index = street_gdf.sindex
     X = np.zeros((len(poi_gdf), nlabels))
     for poi in poi_gdf.itertuples():
         poi_coords = (poi.lon, poi.lat)
-        result_street_idx = list(street_index.nearest(poi_coords))[0]
+        candidates = list(street_index.nearest(poi_coords))
+        nearest = candidates[np.argmin([
+            Point(poi_coords).distance(street_gdf.iloc[c]['geometry'])
+            for c in candidates
+        ])]
         class_cnt = dict((c, 0) for c in range(nlabels))
         result_pois_idxs = [
-            poi_idx for poi_idx in pois_by_street[result_street_idx]
+            poi_idx for poi_idx in pois_by_street[nearest]
             if poi.geometry.distance(geometry_map[poi_idx]) < thr]
         for rpi in result_pois_idxs:
             rpi_label = label_map[rpi]
@@ -72,6 +174,23 @@ def get_classes_in_street_and_radius_cnt(poi_gdf, street_gdf, pois_by_street, nl
 
 
 def get_classes_in_neighbors_bln(poi_gdf, poi_index_path, nlabels, label_map, k):
+    """
+    Creates a features array. For each poi *p* (each row) the array will \
+    contain 1 (True) in column *c*, if there is at least one poi of category \
+    *c* among the *k* nearest neighbors of *p*.
+
+    Args:
+        poi_gdf (geopandas.GeoDataFrame): Contains pois for which the \
+            features will be created
+        poi_index_path (str): Path to the stored index
+        nlabels (int): Number of poi categories
+        label_map (list): A list containing the labels of the train pois
+        k (int): Number of nearest neighbors to take into account
+
+    Returns:
+        numpy.ndarray: The features array of shape (n_samples, n_features), \
+            here (len(poi_gdf), nlabels)
+    """
     poi_index = pickle.load(open(poi_index_path, 'rb'))
     X = np.zeros((len(poi_gdf), nlabels))
     for poi in poi_gdf.itertuples():
@@ -84,6 +203,23 @@ def get_classes_in_neighbors_bln(poi_gdf, poi_index_path, nlabels, label_map, k)
 
 
 def get_classes_in_neighbors_cnt(poi_gdf, poi_index_path, nlabels, label_map, k):
+    """
+    Creates a features array. For each poi *p* (each row) the array will \
+    contain an integer in column *c*, representing the number of pois of \
+    category *c* among the *k* nearest neighbors of *p*.
+
+    Args:
+        poi_gdf (geopandas.GeoDataFrame): Contains pois for which the \
+            features will be created
+        poi_index_path (str): Path to the stored index
+        nlabels (int): Number of poi categories
+        label_map (list): A list containing the labels of the train pois
+        k (int): Number of nearest neighbors to take into account
+
+    Returns:
+        numpy.ndarray: The features array of shape (n_samples, n_features), \
+            here (len(poi_gdf), nlabels)
+    """
     poi_index = pickle.load(open(poi_index_path, 'rb'))
     X = np.zeros((len(poi_gdf), nlabels))
     for poi in poi_gdf.itertuples():
@@ -103,10 +239,14 @@ def get_classes_in_street_radius_bln(poi_gdf, street_gdf, nlabels, label_map, ge
     X = np.zeros((len(poi_gdf), nlabels))
     for poi in poi_gdf.itertuples():
         poi_coords = (poi.lon, poi.lat)
-        result_street_idx = list(street_index.nearest(poi_coords))[0]
+        candidates = list(street_index.nearest(poi_coords))
+        nearest = candidates[np.argmin([
+            Point(poi_coords).distance(street_gdf.iloc[c]['geometry'])
+            for c in candidates
+        ])]
         result_pois_idxs = [
             i for i, geom in enumerate(geometry_map)
-            if street_gdf.iloc[result_street_idx]['geometry'].distance(geom) < thr]
+            if street_gdf.iloc[nearest]['geometry'].distance(geom) < thr]
         for rpi in result_pois_idxs:
             rpi_label = label_map[rpi]
             X[poi.Index][rpi_label] = 1
@@ -118,11 +258,15 @@ def get_classes_in_street_radius_cnt(poi_gdf, street_gdf, nlabels, label_map, ge
     X = np.zeros((len(poi_gdf), nlabels))
     for poi in poi_gdf.itertuples():
         poi_coords = (poi.lon, poi.lat)
-        result_street_idx = list(street_index.nearest(poi_coords))[0]
+        candidates = list(street_index.nearest(poi_coords))
+        nearest = candidates[np.argmin([
+            Point(poi_coords).distance(street_gdf.iloc[c]['geometry'])
+            for c in candidates
+        ])]
         class_cnt = dict((c, 0) for c in range(nlabels))
         result_pois_idxs = [
             i for i, geom in enumerate(geometry_map)
-            if street_gdf.iloc[result_street_idx]['geometry'].distance(geom) < thr]
+            if street_gdf.iloc[nearest]['geometry'].distance(geom) < thr]
         for rpi in result_pois_idxs:
             rpi_label = label_map[rpi]
             class_cnt[rpi_label] += 1
--- LGM-Classification/osm_utilities.py
+++ LGM-Classification/osm_utilities.py
@@ -1,11 +1,20 @@
 import pandas as pd
-import re
 import json
 import requests
-from shapely.geometry import LineString, Polygon
+from shapely.geometry import LineString
 
 
 def query_osm_data(query, fpath):
+    """
+    Queries Overpass API for *query*.
+
+    Args:
+        query (str): The query to be passed to API
+        fpath (str): File path to write the API response
+
+    Returns:
+        None
+    """
     overpass_url = 'http://overpass-api.de/api/interpreter'
     response = requests.get(overpass_url, params={'data':query}).json()
     with open(fpath, 'w') as f:
@@ -14,10 +23,21 @@ def query_osm_data(query, fpath):
 
 
 def parse_osm_streets(fpath):
+    """
+    Parses the API response from *fpath* and converts it to a dataframe.
+
+    Args:
+        fpath (str): File path to read
+
+    Returns:
+        pandas.DataFrame: Contains all streets as well as their geometries
+    """
     # Helper function
     def convert_to_wkt_geometry(row):
         lons = [p['lon'] for p in row['geometry']]
         lats = [p['lat'] for p in row['geometry']]
+        if len(lons) < 2 or len(lats) < 2:
+            return None
         return LineString(list(zip(lons, lats)))
 
     with open(fpath, encoding='utf-8') as f:
@@ -27,10 +47,23 @@ def parse_osm_streets(fpath):
     cols = ['id', 'geometry']
     street_df = pd.DataFrame(data=data, columns=cols)
     street_df['geometry'] = street_df.apply(convert_to_wkt_geometry, axis=1)
+    street_df = street_df.dropna()
     return street_df
 
 
 def download_osm_streets(bbox_coords, exp_path):
+    """
+    Queries Overpass API for streets inside *bbox_coords* and saves them into \
+    a csv file.
+
+    Args:
+        bbox_coords (tuple): Contains the bounding box coords to download \
+            from the API in (south, west, north, east) format
+        exp_path (str): Path to write
+
+    Returns:
+        None
+    """
     fpath = exp_path + '/osm_streets.json'
     query = (
         '[out:json]'
@@ -44,44 +77,44 @@ def download_osm_streets(bbox_coords, exp_path):
     return
 
 
-def parse_osm_polys(fpath):
-    # Helper function
-    def extract_name_tags(row):
-        names = [tag[1] for tag in row['tags'] if re.search('name', tag[0])]
-        return names
+# def parse_osm_polys(fpath):
+#     # Helper function
+#     def extract_name_tags(row):
+#         names = [tag[1] for tag in row['tags'] if re.search('name', tag[0])]
+#         return names
 
-    # Helper function
-    def convert_to_wkt_geometry(row):
-        lons = [p['lon'] for p in row['geometry']]
-        lats = [p['lat'] for p in row['geometry']]
-        return Polygon(list(zip(lons, lats)))
+#     # Helper function
+#     def convert_to_wkt_geometry(row):
+#         lons = [p['lon'] for p in row['geometry']]
+#         lats = [p['lat'] for p in row['geometry']]
+#         return Polygon(list(zip(lons, lats)))
 
-    with open(fpath, encoding='utf-8') as f:
-        polys = json.load(f)['elements']
+#     with open(fpath, encoding='utf-8') as f:
+#         polys = json.load(f)['elements']
 
-    data = []
-    for poly in polys:
-        if 'tags' in poly:
-            poly_tags = [(k, v) for k, v in poly['tags'].items()]
-            data.append((poly['id'], poly_tags, poly['geometry']))
+#     data = []
+#     for poly in polys:
+#         if 'tags' in poly:
+#             poly_tags = [(k, v) for k, v in poly['tags'].items()]
+#             data.append((poly['id'], poly_tags, poly['geometry']))
 
-    cols = ['id', 'tags', 'geometry']
-    poly_df = pd.DataFrame(data=data, columns=cols)
-    poly_df['name'] = poly_df.apply(extract_name_tags, axis=1)
-    poly_df['geometry'] = poly_df.apply(convert_to_wkt_geometry, axis=1)
-    return poly_df
+#     cols = ['id', 'tags', 'geometry']
+#     poly_df = pd.DataFrame(data=data, columns=cols)
+#     poly_df['name'] = poly_df.apply(extract_name_tags, axis=1)
+#     poly_df['geometry'] = poly_df.apply(convert_to_wkt_geometry, axis=1)
+#     return poly_df
 
 
-def download_osm_polygons(bbox_coords, exp_path):
-    fpath = exp_path + '/osm_polys.json'
-    query = (
-        '[out:json]'
-        f'[bbox:{bbox_coords[0]},{bbox_coords[1]},{bbox_coords[2]},{bbox_coords[3]}];'
-        'way(if:is_closed());'
-        'out geom;')
-    query_osm_data(query, fpath)
-    poly_df = parse_osm_polys(fpath)
-    fpath = exp_path + '/osm_polys.csv'
-    cols = ['id', 'name', 'tags', 'geometry']
-    poly_df.to_csv(f'{fpath}', columns=cols, index=False)
-    return
+# def download_osm_polygons(bbox_coords, exp_path):
+#     fpath = exp_path + '/osm_polys.json'
+#     query = (
+#         '[out:json]'
+#         f'[bbox:{bbox_coords[0]},{bbox_coords[1]},{bbox_coords[2]},{bbox_coords[3]}];'
+#         'way(if:is_closed());'
+#         'out geom;')
+#     query_osm_data(query, fpath)
+#     poly_df = parse_osm_polys(fpath)
+#     fpath = exp_path + '/osm_polys.csv'
+#     cols = ['id', 'name', 'tags', 'geometry']
+#     poly_df.to_csv(f'{fpath}', columns=cols, index=False)
+#     return
